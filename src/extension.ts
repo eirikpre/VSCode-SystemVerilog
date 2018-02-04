47af71286de9d71c358f67ded64cc12ea0152d34
Eirik Prestegårdshus <eirikpre@gmail.com>
2018-02-04 22:09:52 +0100

    Attempt at DocumentHighlightProvider

--- src/extension.ts
+++ src/extension.ts
@@ -4,18 +4,30 @@
 import * as vscode from 'vscode';
 import { SystemVerilogDocumentSymbolProvider } from './providers/DocumentSymbolProvider';
 import { SystemVerilogWorkspaceSymbolProvider } from './providers/WorkspaceSymbolProvider';
+import SystemVerilogDocumentHighlightProvider from './providers/DocumentHighlightProvider';
+
+
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
+    let documentSelector = ['verilog', 'systemverilog']
+
     context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(
-        {language: "systemverilog"}, new SystemVerilogDocumentSymbolProvider()
+        documentSelector,
+        new SystemVerilogDocumentSymbolProvider()
     ));
 
     context.subscriptions.push(vscode.languages.registerWorkspaceSymbolProvider(
         new SystemVerilogWorkspaceSymbolProvider()
     ));
+
+    // Built-in DocumentHighlightProvider is better
+    // context.subscriptions.push(vscode.languages.registerDocumentHighlightProvider(
+    //    documentSelector,
+    //    new SystemVerilogDocumentHighlightProvider()
+    // ));
 
     console.log("Extension SystemVerilog loaded successfully")
 }
