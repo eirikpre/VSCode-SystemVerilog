fa4342ae765ae0ebf8844d41ef4e8c38709711b9
Eirik Prestegårdshus <eirikpre@gmail.com>
2018-02-05 22:38:22 +0100

    Added search that matches every instantiation as symbols

--- src/providers/DocumentSymbolProvider.ts
+++ src/providers/DocumentSymbolProvider.ts
@@ -3,7 +3,7 @@ import {DocumentSymbolProvider, SymbolInformation, CancellationToken, TextDocume
 
 
 export class SystemVerilogDocumentSymbolProvider implements DocumentSymbolProvider {
-    public regex = /^\s*(logic|class|program|module|task|function|package|interface)(?:\s+(?:automatic|static|void|int))?\s+(\w+)/;
+    public regex: RegExp = /^\s*(logic|class|program|module|task|function|package|interface)(?:\s+(?:automatic|static|void|int))?\s+(\w+)/;
     private _inst_regex = /^\s*(?:virtual)?\s*(\w+)\s*(\w+)\s*;/;
 
     public varToSymbolKind: { [key: string]: SymbolKind } = {
@@ -22,47 +22,64 @@ export class SystemVerilogDocumentSymbolProvider implements DocumentSymbolProvid
     public provideDocumentSymbols(document: TextDocument, token: CancellationToken): Thenable<SymbolInformation[]> {
         return new Promise((resolve, reject) => {
             var symbols = [];
+            // for (let i = 0; i < document.lineCount; i++) {
+            //     let line = document.lineAt(i);
+            //     let match = this.regex.exec(line.text);
+            //     let match2 = this._inst_regex.exec(line.text);
 
-            for (let i = 0; i < document.lineCount; i++) {
-                let line = document.lineAt(i);
-                let match = this.regex.exec(line.text);
-                let match2 = this._inst_regex.exec(line.text);
-
-                if (match !== null) {
-                    symbols.push(new SymbolInformation(
-                        match[2],
-                        this.varToSymbolKind[match[1]],
-                        match[1],
-                        new Location(document.uri, new Position(line.lineNumber, line.text.indexOf(match[2])))
-                    ));
-                } else if (match2 !== null) {
-                    symbols.push(new SymbolInformation(
-                        match2[2],
-                        SymbolKind.Module,
-                        match2[1],
-                        new Location(document.uri, new Position(line.lineNumber, line.text.indexOf(match2[2])))
-                    ));
-                }
-            }
+            //     if (match !== null) {
+            //         symbols.push(new SymbolInformation(
+            //             match[2],
+            //             this.varToSymbolKind[match[1]],
+            //             match[1],
+            //             new Location(document.uri, new Position(line.lineNumber, line.text.indexOf(match[2])))
+            //         ));
+            //         names.push(match[2]);
+            //     } else if (match2 !== null) {
+            //         symbols.push(new SymbolInformation(
+            //             match2[2],
+            //             SymbolKind.Module,
+            //             match2[1],
+            //             new Location(document.uri, new Position(line.lineNumber, line.text.indexOf(match2[2])))
+            //         ));
+            //         names.push(match2[2]);
+            //     }
+            // }
             let text = document.getText()
-            let r_multiline = new RegExp('^\\s*(\\w+)[\\s\\n\\r]*(?:#[\\s\\n\\r]*\\([\\n\\r.]*\\))?[\\s\\n\\r]*(\\w+)[\\s\\n\\r]*\\([\\n\\r.]*?\\)\\s*;','mg');
-            // let r_multiline = new RegExp('^\\s*(\\w+)[\\s\\n\\r]*(?>#[\\s\\n\\r]*(?>(?>'open'\\(.*)+(?>'-open'\\)+)))(\\w+)[\\s\\n\\r]*\\([\\s\\S]*?\\)\\s*;','mg');
-            // Attempt at balancing group
-            // ^(?:(?'open'o)+(?'-open'c)+)+(?(open)(?!))$
+            let r_multiline: RegExp = new RegExp('^\\s*(\\w+)(?:\\s+|#\\s*\\([\\s\\S]*?\\))(\\w+)\\s*(?:\\([\\s\\S]*?\\))?\\s*;','mg');
             var match;
             do {
-                match = r_multiline.exec(text)
+                match = r_multiline.exec(text);
                 if (match) {
-                    if (symbols.indexOf(match[2]) == -1) {
-                        symbols.push(new SymbolInformation(
-                            match[2],
-                            SymbolKind.Module,
-                            match[1],
-                            new Location(document.uri, new Position(0,0))
-                        ));
-                    }
+                    symbols.push(new SymbolInformation(
+                        match[2],
+                        SymbolKind.Module,
+                        match[1],
+                        new Location(document.uri, new Range(0,0,0,0))
+                    ));
                 }
             } while (match != null);
+            // TODO: Find container name and inside comment block
+            // XXX: Does not match multiple in sequence, eg. logic a, b;
+            var line_no: number = 0;
+            symbols.forEach( function(symbol) {
+                let word = symbol.name;
+                let regex: RegExp = new RegExp('\\b(word)\\b'.replace('word', word))
+                while (line_no < document.lineCount) {
+                    let line = document.lineAt(line_no).text;
+                    let commentStart = line.indexOf('//');
+                    if (commentStart != -1) {
+                        line = line.substr(0, commentStart);
+                    }
+                    let match = regex.exec(line);
+                    if (match) {
+                        symbol.name = "type word".replace('type', symbol.containerName).replace('word', symbol.name);
+                        symbol.location.range = new Range(line_no, match.index, line_no, match.index+word.length);
+                        break;
+                    }
+                    line_no++;
+                }
+            })
             resolve(symbols);
         });
     }
