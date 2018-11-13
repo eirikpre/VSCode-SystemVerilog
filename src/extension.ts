77c95d600fd1e238822975d0ca5d145100a2f0b8
eirikpre <eirik.prestegardshus@nordicsemi.no>
2018-11-13 11:22:34 +0100

    Improvements to DocSymProvider and debuging node

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,24 @@
 'use strict';
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { SystemVerilogDocumentSymbolProvider, SystemVerilogDocumentSymbolTreeProvider } from './providers/DocumentSymbolProvider';
-import { SystemVerilogWorkspaceSymbolProvider } from './providers/WorkspaceSymbolProvider';
-import SystemVerilogDocumentHighlightProvider from './providers/DocumentHighlightProvider';
-import { SystemVerilogTreeDataProvider } from './providers/TreeDataProvider';
+// import * as vscode from 'vscode';
+import { workspace, window, languages, commands, StatusBarAlignment, DocumentSelector, ExtensionContext} from 'vscode';
 import { SystemVerilogDefinitionProvider } from './providers/DefintionProvider';
-import {SystemVerilogHoverProvider} from './providers/HoverProvider';
+import { SystemVerilogDocumentSymbolProvider } from './providers/DocumentSymbolProvider';
+import { SystemVerilogHoverProvider } from './providers/HoverProvider';
+import { SystemVerilogWorkspaceSymbolProvider } from './providers/WorkspaceSymbolProvider';
 
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-    const settings = vscode.workspace.getConfiguration();
-    let selector:vscode.DocumentSelector = [{ scheme: 'file', language: 'systemverilog' }, { scheme: 'file', language: 'verilog' }];
+export function activate(context: ExtensionContext) {
+    const settings = workspace.getConfiguration();
+    let selector:DocumentSelector = [{ scheme: 'file', language: 'systemverilog' }, { scheme: 'file', language: 'verilog' }];
     
     // TODO: Add setting to turn off indexing.
     // (To reduce RAM/CPU usage)
     
-    let statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
+    let statusBar = window.createStatusBarItem(StatusBarAlignment.Left, 0);
     statusBar.text = 'SystemVerilog: Active';
     statusBar.show();
     statusBar.command = 'systemverilog.build_index';
@@ -30,17 +29,17 @@ export function activate(context: vscode.ExtensionContext) {
     let hoverProvider = new SystemVerilogHoverProvider(symProvider, docProvider);
 
     context.subscriptions.push(statusBar);
-    context.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(selector, docProvider));
-    context.subscriptions.push(vscode.languages.registerDefinitionProvider(selector, defProvider));
-    context.subscriptions.push(vscode.languages.registerHoverProvider(selector, hoverProvider));
-    context.subscriptions.push(vscode.languages.registerWorkspaceSymbolProvider(symProvider));
-    context.subscriptions.push(vscode.commands.registerCommand('systemverilog.build_index', rebuild));
+    context.subscriptions.push(languages.registerDocumentSymbolProvider(selector, docProvider));
+    context.subscriptions.push(languages.registerDefinitionProvider(selector, defProvider));
+    context.subscriptions.push(languages.registerHoverProvider(selector, hoverProvider));
+    context.subscriptions.push(languages.registerWorkspaceSymbolProvider(symProvider));
+    context.subscriptions.push(commands.registerCommand('systemverilog.build_index', rebuild));
     // WIP
-    // vscode.window.registerTreeDataProvider('systemverilogModules', new SystemVerilogTreeDataProvider())
-    // vscode.window.registerTreeDataProvider('systemverilogDocuementSymbols', new SystemVerilogDocumentSymbolTreeProvider())
+    // window.registerTreeDataProvider('systemverilogModules', new SystemVerilogTreeDataProvider())
+    // window.registerTreeDataProvider('systemverilogDocuementSymbols', new SystemVerilogDocumentSymbolTreeProvider())
 
     // Built-in DocumentHighlightProvider is better
-    // context.subscriptions.push(vscode.languages.registerDocumentHighlightProvider(selector, new SystemVerilogDocumentHighlightProvider()));
+    // context.subscriptions.push(languages.registerDocumentHighlightProvider(selector, new SystemVerilogDocumentHighlightProvider()));
 
     console.log("Extension SystemVerilog loaded successfully")
 
--- src/providers/DocumentSymbolProvider.ts
+++ src/providers/DocumentSymbolProvider.ts
@@ -1,6 +1,5 @@
 import {DocumentSymbolProvider, SymbolInformation, CancellationToken, TextDocument, Location, Position, SymbolKind, Range, TreeDataProvider, TreeItem,
         Event, EventEmitter, TreeItemCollapsibleState, window} from 'vscode'
-
 // See test/SymbolKind_icons.png for an overview of the icons
 export function getSymbolKind(name: String): SymbolKind {
     switch (name) {
@@ -34,86 +33,57 @@ export function getSymbolKind(name: String): SymbolKind {
 }
 
 export class SystemVerilogDocumentSymbolProvider implements DocumentSymbolProvider {
-
     // XXX: Does not match virtual interface instantiantion, eg virtual intf u_virtInterface;
     // XXX: Does not match input/output/inout ports, eg input logic din, ..
     // TODO: Match labels with SymbolKind.Enum
-    public regex: RegExp = new RegExp('^\\s*(?!return|begin|end|else|join|fork|for|if)(?:(?:virtual|static|automatic)\\s+)?([:\\w]+)(?:\\s+(?:virtual|static|automatic))?(?:\\s+|\\s*#\\s*\\([\\s\\S]*?\\)\\s*|\\s+\\w+\\s+)(?:\\[.*?\\]\\s*)?(?!for|if|return)(\\w+(?:\\s*,\\s*\\w+)*?)\\s*(?:\\([\\s\\S]*?\\)|extends\\s*\\w+)?\\s*;','mg');
+    public regex: RegExp = new RegExp ([
+        ,/(?:\s(?:virtual|static|automatic)\s*)?/                       // Block level modifiers
+        ,/(?:\s)/                                                       // Whitespace
+        ,/(?!return|begin|end|else|join|fork|for|if|virtual|static|automatic)/ // Illegal symbol types
+        
+        ,/([:\w]+)/                                                     // Symbol type
+
+        ,/(?:\s+(?:virtual|static|automatic)?(?:\s*\[.*?\])?\s*\w+)?/   // Return type
+
+        ,/\s+(\w+)(?:\s*,\s*\w+)*?/                                     // Symbol name
+
+        ,/(?:\(.*?\)|extends\s*\w+|\s*#\s*\(.*?\)|)*?/                  // Portlist / class suffix
+
+        ,/;/                                                            // End of definition
+        ].map(x => x.source).join(''), 'mg');
+
+        // FIXME: Update when VS Code upgrades to Chome 62 for PCRE Regex'es!
+
+        // public regex_block: RegExp = new RegExp ([
+        //      /(?<=\s)(function\s+\w+|task|class|module|program|package)\s+/ // Symbol type
+        //     ,/(\w+)/
+        //     ,/[\w\W.]*?/
+        //     ,/(end\1)/
+        //     ].map(x => x.source).join(''), 'mg');
 
     public provideDocumentSymbols(document: TextDocument, token?: CancellationToken): Thenable<SymbolInformation[]> {
         return new Promise((resolve, reject) => {
-            /* 
-                First loop finds every instantiation of 
-                variables/modules/objects in the file
-            */
             var symbols = [];
-            let text = document.getText()
             var match;
+            let text = document.getText();
+
+            /* 
+                Matches the regex and uses the index from the regex to find the position
+            */
             do {
                 match = this.regex.exec(text);
                 if (match) {
-                    let words = match[2].match(/\b(\w+)\b/g)
-                    words.forEach( object => {
-                        symbols.push(new SymbolInformation(
-                            object,
-                            getSymbolKind(match[1]),
-                            match[1],
-                            new Location(document.uri, new Range(0,0,0,0))
-                        ));
-                    });
+                    symbols.push(new SymbolInformation(
+                        match[2],
+                        getSymbolKind(match[1]),
+                        match[1],
+                        new Location(document.uri,
+                            new Range(document.positionAt(match.index+1),
+                                      document.positionAt(match.index+match[0].length)
+                    ))));
                 }
             } while (match != null);
 
-            /*
-                Second loop finds the position of every symbol
-                and sets name based on containerName and TODO setup containerName correctly
-            */
-            // TODO: Inside comment block (track current scope)
-            // TODO: May match wrongly if the same name is used before declaration
-            // FIXME: Use symbol before declaration matches the use rather than declaration! eg. functions
-            var scope: string[] = [""];
-            var scopeType: string[] = [""];
-            // var commentBlock: Boolean = false;
-            var line_no: number = 0;
-            var notFound = [];
-            symbols.forEach( function(symbol) {
-                let word = symbol.name;
-                let type = symbol.containerName;
-                let name = symbol.name;
-                let found = false;
-                let regex: RegExp;
-                if ( "module|program|class|function|task|interface|config|package".match("\\b"+type+"\\b")) {
-                    regex = new RegExp('(?:^\\s*?(?:virtual\\s+(?=class))?type.*?[^\\.])\\b(word)\\b'.replace('word', word).replace('type', type));
-                } else {
-                    regex = new RegExp('(?:^.*?[^\\.])\\b(word)\\b'.replace('word', word));
-                }
-                while (line_no < document.lineCount) {
-                    let line = document.lineAt(line_no).text;
-                    let commentStart = line.indexOf('//');
-                    if (commentStart != -1) {
-                        line = line.substr(0, commentStart);
-                    }
-                    let match = regex.exec(line);
-                    if (match) {
-                        symbol.location.range = new Range(line_no, match.index, line_no, match.index+word.length);
-                        symbol.containerName = scope[scope.length-1];
-                        if ( "module|program|class|function|task|interface|config|package".match("\\b"+type+"\\b")){
-                            scope.push(name);
-                            scopeType.push(type);
-                        }
-                        found = true;
-                        break;
-                    }
-                    if (line.indexOf("end" + scopeType[scopeType.length-1]) != -1){
-                        scope.pop();
-                        scopeType.pop();
-                    }
-                    line_no++;
-                }
-                if (!found) {
-                    notFound.push(symbol);
-                }
-            });
             resolve(symbols);
         });
     }
