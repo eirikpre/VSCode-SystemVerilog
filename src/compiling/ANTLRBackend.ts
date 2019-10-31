b043d225f2272dfad26879f4500d441588abba6a
bhan2 <bhan2@ncsu.edu>
2019-10-30 20:19:15 -0400

    Removed ANTLRCompiler and added changes to ANTLRBackend after merging develop branch(file name changed).\

--- src/compiling/ANTLRBackend.ts
+++ src/compiling/ANTLRBackend.ts
@@ -35,9 +35,12 @@ export class ANTLRBackend{
             let visitedDocuments = new Map<string, boolean>();
             let diagnosticCollection: Map<string, Diagnostic[]> = new Map();
 
-            // Create the lexer and parser
+            // Get document text
             let text = document.getText();
-            let inputStream = new ANTLRInputStream(document.getText());
+            // Perform macro replacements
+            let new_text = this.macroReplace(text);
+            // Create the lexer and parser
+            let inputStream = new ANTLRInputStream(new_text);   
             let lexer = new SystemVerilogLexer(inputStream);
             let tokenStream = new CommonTokenStream(lexer);
             let parser = new SystemVerilogParser(tokenStream);
@@ -92,8 +95,63 @@ export class ANTLRBackend{
                 out = ""; //filter out all errors for mismatched input
             else
                 out = 'mismatched input "' + parser_error.offendingSymbol.text + '"';
-
         }
         return out;
+    }
+    /**
+        Function for replacing macro uses with their appropriate text
+        @param text The text to identify macro definitions and replace macro uses within
+        @returns The text with macro definitions removed and their uses replaced with the text they represent
+    */
+    public macroReplace(text: string): string {
+        let defines_with_text: [[string, string][], string] = this.extract_defines(text.replace(/\r\n/g, '\n'));
+        let defines: [string, string][] = defines_with_text[0];
+        let new_text: string = defines_with_text[1];
+
+        return this.replace_defines(new_text, defines);
+    }
+
+    /**
+        Function for identifying macro definitions and removing them from the text
+        @param text The text to identify macro definitions and replace macro uses within
+        @returns The array of macro labels and the text they represent, and the full text with the macro definitions removed
+    */
+    private extract_defines(text: string): [[string, string][], string] {
+        let current_index: number = text.indexOf('`define');
+        let defines: [string, string][] = [];
+        let new_text: string = text.slice(0, current_index);
+        while (current_index != -1) {
+            let label: string = text.split(" ", 2)[1];
+            let temp_index: number = text.indexOf('\n', current_index);
+            while (temp_index != -1 && text.charAt(temp_index - 1) == '\\') {
+                temp_index = text.indexOf('\n', temp_index + 1);
+            }
+            let value: string = text.slice(text.indexOf(label, current_index) + label.length + 1, temp_index);
+            value = value.replace('\\\n', '\n');
+            defines.push([label, value]);
+            current_index = text.indexOf('`define', current_index + 1);
+            if (current_index == -1) {
+                new_text = new_text.concat(text.slice(temp_index + 1));
+            } else {
+                new_text = new_text.concat(text.slice(temp_index + 1, current_index));
+            }
+        }
+        return [defines, new_text];
+    }
+
+    /**
+        Function for replacing the appearances of defined macros with their appropriate text within the full text
+        @param text The text to replace the macro uses within
+        @param defines The array of macro labels and the text they represent
+        @returns The full text, with macro uses replaced with the text they represent
+    */
+    private replace_defines(text: string, defines: [string, string][]): string {
+        let new_text: string = text;
+        defines.forEach(function (define) {
+            while (new_text.indexOf('`'+define[0]) != -1) {
+                new_text = new_text.replace('`' + define[0], define[1]);
+            }        
+        });
+        return new_text;
     }
 };
