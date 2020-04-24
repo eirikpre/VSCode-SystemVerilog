1e2f44e22d1c0f77e30520893abb7d4945351faa
eipr <eirik.prestegardshus@nordicsemi.no>
2020-04-24 13:37:21 +0200

    Removed debug print

--- src/providers/DocumentSymbolProvider.ts
+++ src/providers/DocumentSymbolProvider.ts
@@ -18,18 +18,17 @@ export class SystemVerilogDocumentSymbolProvider implements DocumentSymbolProvid
 
     /**
         Matches the regex pattern with the document's text. If a match is found, it creates a `SystemVerilogSymbol` object.
-        If `documentSymbols` is not `undefined`, than the object is added to it, 
+        If `documentSymbols` is not `undefined`, than the object is added to it,
         otherwise add the objects to an empty list and return it.
-        
+
         @param document The document in which the command was invoked.
         @param token A cancellation token.
         @return A list of `SystemVerilogSymbol` objects or a thenable that resolves to such. The lack of a result can be
         signaled by returning `undefined`, `null`, or an empty list.
     */
     public provideDocumentSymbols(document: TextDocument, token?: CancellationToken): Thenable<Array<SystemVerilogSymbol>> {
-        console.debug("provideDocumentSymbols!",document.uri.path);
         return new Promise((resolve) => {
-            /* 
+            /*
             Matches the regex and uses the index from the regex to find the position
             TODO: Look through the symbols to check if it either is defined in the current file or in the workspace.
                   Use that information to figure out if an instanciated 'unknown' object is of a known type.
