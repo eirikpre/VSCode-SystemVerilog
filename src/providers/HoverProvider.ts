32c2865b7b44bc8483b82192ebc2a310cb92a2e2
eipr <eirik.prestegardshus@nordicsemi.no>
2020-05-04 14:15:07 +0200

    trim hover string

--- src/providers/HoverProvider.ts
+++ src/providers/HoverProvider.ts
@@ -5,16 +5,16 @@ export class SystemVerilogHoverProvider implements HoverProvider {
     provideHover(document : TextDocument, position : Position, token: CancellationToken) : ProviderResult<Hover> {
         return new Promise( (resolve, reject) => {
             var lookupRange = document.getWordRangeAtPosition(position);
-            
+
             if (!lookupRange) {
                 return resolve(undefined);
             }
-            
+
             resolve(commands.executeCommand(
                 "vscode.executeDefinitionProvider", document.uri, position, token)
                 .then((loc: Location[]): Thenable<string> => {
                     return workspace.openTextDocument(loc[0].uri).then(doc => {
-                        return doc.lineAt(loc[0].range.start.line).text;
+                        return doc.lineAt(loc[0].range.start.line).text.trim();
                     });
                 }).then((str: string): Hover => {
                     return new Hover([{
