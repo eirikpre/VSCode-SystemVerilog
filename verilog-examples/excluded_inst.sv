995eb0022292619ef4fc6e5457d75ac6820d9a4d
eipr <eirik.prestegardshus@nordicsemi.no>
2021-01-18 13:25:03 +0100

    new folding matches fixes #65

--- verilog-examples/excluded_inst.sv
+++ verilog-examples/excluded_inst.sv
@@ -1,5 +1,39 @@
+// Folding example that should work
+`ifdef A
+  A_TEST_ONE;
+  A_TEST_TWO;
+
+`ifdef B
+  B_TEST_ONE;
+  B_TEST_TWO;
+  `endif
+
+`ifdef  C
+  C_TEST_ONE;
+  C_TEST_TWO;
+  `endif
+
+A_TEST_ONE;
+A_TEST_TWO;
+`endif
+
+// Folding example that does not work
+`ifdef A
+  A_TEST_ONE;
+  A_TEST_TWO;
+
+`elsif B
+  B_TEST_ONE;
+  B_TEST_TWO;
+
+`else  C
+  C_TEST_ONE;
+  C_TEST_TWO;
+
+`endif
 
 
+// Excluded from indexing example
 module excluded #(
   parameter A
 )(
