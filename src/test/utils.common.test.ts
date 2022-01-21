996af38c152bd4626aa48cf2c7c3f77e4dddd4d1
Joe Crop <joe.crop@gmail.com>
2022-01-21 08:39:06 -0800

    updated test cases and getPathFromUri() to handle Windows paths

--- src/test/utils.common.test.ts
+++ src/test/utils.common.test.ts
@@ -2,19 +2,35 @@ import * as assert from 'assert';
 import { getPathFromUri } from '../utils/common';
 
 suite('Utils Common Tests', () => {
-    test('test #1: getPathFromUri', async () => {
+    test('test #1: getPathFromUri-file', async () => {
         let rootPath = 'c:/home/users/workspace';
-
         let expectedPath = 'c:/home/users/workspace/directory/design.sv';
         let uri = 'file:///c%3A/home/users/workspace/directory/design.sv';
 
         assert.strictEqual(expectedPath, getPathFromUri(uri, rootPath));
 
-        rootPath = '/home/users/workspace';
-        expectedPath = '/home/users/workspace/directory/design.sv';
-        uri = 'file:///home/users/workspace/directory/design.sv';
+    });
+
+    test('test #2: getPathFromUri-windows', async () => {
+
+        let rootPath = 'c:\\home\\users\\workspace';
+        let expectedPath = 'c:\\home\\users\\workspace\\directory\\design.sv';
+        let uri = 'c:\\home\\users\\workspace\\directory\\design.sv';
+        let got = getPathFromUri(uri, rootPath);
+        assert.strictEqual(expectedPath, got);
+
+    });
+
+    test('test #3: getPathFromUri-linux', async () => {
+
+        let rootPath = '/home/users/workspace';
+        let expectedPath = '/home/users/workspace/directory/design.sv';
+        let uri = 'file:///home/users/workspace/directory/design.sv';
 
         assert.strictEqual(expectedPath, getPathFromUri(uri, rootPath));
+    });
+
+    test('test #4: getPathFromUri-undef', async () => {
 
         // URI without a path
         assert.strictEqual('', getPathFromUri('file:///', ''));
--- src/utils/common.ts
+++ src/utils/common.ts
@@ -11,18 +11,33 @@ export function getPathFromUri(uri: string, rootPath: string): string {
     if (!uri || !rootPath) {
         return '';
     }
+    let matches;
 
+    
     const lUri = decodeURIComponent(uri); // convert hexadecimal characters to ASCII
-    const parsedUri = uriJs.parse(lUri);
-    if (!parsedUri.path) {
+    // don't parse if it is a windows path starting with a single letter followed by a colon
+    const dontParseUri = new RegExp(`^\[a-zA-Z]:.*`);
+    let parsedPath = null;
+    let windowsPath = false;
+    if ((matches = dontParseUri.exec(lUri)) != null && matches.length == 1) {
+        parsedPath = uri.replace(/\\/g, '/');;
+        windowsPath = true;
+    } else {
+        const parsedUri = uriJs.parse(lUri);
+        parsedPath = parsedUri.path;
+    }
+    if (!parsedPath) {
         return '';
     }
 
-    let matches;
     const lRootPath = rootPath.replace(/\\/g, '/');
     const regex = new RegExp(`/?${lRootPath}(.*)`);
-    if ((matches = regex.exec(parsedUri.path)) != null && matches.length > 1) {
-        return lRootPath + matches[1];
+    if ((matches = regex.exec(parsedPath)) != null && matches.length > 1) {
+        let foundPath = lRootPath + matches[1];
+        if(windowsPath) {
+            return foundPath.replace(/\//g, '\\');
+        }
+        return foundPath;
     }
 
     return '';
