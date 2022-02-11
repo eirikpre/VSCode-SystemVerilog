a4b4c0d336b337813f9560a3f961483b4974aa54
Joe Crop <joe.crop@gmail.com>
2022-02-10 20:47:46 -0800

    registering symbol provider in test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -3,6 +3,8 @@ import * as path from 'path';
 import * as vscode from 'vscode';
 import { CancellationToken, CancellationTokenSource } from 'vscode-languageclient';
 import * as definitionProvider from '../providers/DefinitionProvider';
+import { SystemVerilogDocumentSymbolProvider } from '../providers/DocumentSymbolProvider';
+import { SystemVerilogParser } from '../parser';
 
 const examplesFolderLocation = '../../verilog-examples';
 const testFolderLocation = '../../src/test/test-files';
@@ -31,6 +33,10 @@ suite('Extension Tests', () => {
     });
 
     test('test #3: DefinitionProvider Non-Illegal Symbol', async () => {
+        const selector: vscode.DocumentSelector = [
+            { scheme: 'file', language: 'systemverilog' },
+            { scheme: 'file', language: 'verilog' }
+        ];
         const uri = vscode.Uri.file(path.join(__dirname, testFolderLocation, 'illegal_symbols.sv'));
         const document = await vscode.workspace.openTextDocument(uri);
 
@@ -38,6 +44,9 @@ suite('Extension Tests', () => {
         const symbolPosition = new vscode.Position(15,9); // assign return_true
                                                           //        ^
 
+        const parser = new SystemVerilogParser();
+        const docProvider = new SystemVerilogDocumentSymbolProvider(parser);
+        vscode.languages.registerDocumentSymbolProvider(selector, docProvider)
         let defProvider = new definitionProvider.SystemVerilogDefinitionProvider();
         let tokenSource = new CancellationTokenSource();
         const definition: vscode.Definition = await defProvider.provideDefinition(document, symbolPosition, tokenSource.token)
@@ -54,7 +63,7 @@ suite('Extension Tests', () => {
         const document = await vscode.workspace.openTextDocument(uri);
 
         // Range of the module in the document
-        const symbolPosition = document.positionAt(243);
+        const symbolPosition = new vscode.Position(8,2);
 
         let defProvider = new definitionProvider.SystemVerilogDefinitionProvider();
         let tokenSource = new CancellationTokenSource();
