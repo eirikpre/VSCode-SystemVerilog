6ae13fea761772782891adc0e0c5f2bda9ec1e3d
richzwart <rich.zwart@gmail.com>
2024-01-05 21:37:35 -0500

    Fixed dropping spaces in comments

--- src/providers/ModuleInstantiator.ts
+++ src/providers/ModuleInstantiator.ts
@@ -430,7 +430,10 @@ function parseContainer(symbol: string, container: string, moduleIsParameterized
     }
 
     const output = [];
-    // Filtering out keys beforehand makes it slightly less time consuming to debug
+
+    const keys = container.split(' ');
+    // Filtering out keys beforehand makes it slightly less time consuming to debug but also breaks the comment grabber so don't do this: 
+    /*
     const keys = container.split(' ').filter((el) => {
         if (isEmptyKey(el)) {
             return false;
@@ -438,6 +441,7 @@ function parseContainer(symbol: string, container: string, moduleIsParameterized
             return true;
         }
     });
+    */
 
     // This is a bit funky, but the for loop below actually checks if these varaibles
     // are undefined so it is important that they are initialized as such
