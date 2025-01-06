3a7c1382b116d0d13977eaf6487b00bfbc4823ce
Joe Crop <joe.crop@gmail.com>
2025-01-06 10:50:19 -0800

    60s tests

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -22,11 +22,10 @@ async function main() {
             extensionTestsPath,
             launchArgs: [workspacePath, '--disable-workspace-trust', '--disable-extensions'],
             version: 'stable',
-            timeout: 30000 // 30 seconds (default was 10)
+            timeout: 60000 // 30 seconds (default was 10)
         });
     } catch (err) {
         console.error(err);
-        console.error(err.message);
         console.error('Failed to run tests');
         process.exit(1);
     }
